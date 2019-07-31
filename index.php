5da2953ec7226bc9241d00310360abdca8f30f69
newiscool <alandiego.dantas@hotmail.com>
2019-07-31 11:31:35 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!--- Basic Page Needs  -->
+    <meta charset="utf-8">
+    <title>Paulo Razz</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Mobile Specific Meta  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- CSS -->
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/slicknav.min.css">
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/magnificpopup.css">
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/jquery.mb.YTPlayer.min.css">
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/typography.css">
+    <link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/style.css">
+	<link rel="stylesheet" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/css/responsive.css">
+	
+	<!-- http://localhost/paulorazz/wp-content/themes/twentynineteen/style.css?ver=1.0 -->
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/img/icon/favicon.ico">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+
+<body>
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="spinner"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- header area start -->
+    <header id="header">
+        <div class="header-area">
+            <div class="container">
+                <div class="row">
+                    <div class="menu-area">
+                        <div class="col-md-3 col-sm-12 col-xs-12">
+                            <div class="logo">
+                                <a href="index.html"><img src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/img/icon/logo.png" alt="Zeedapp - App Landing Template"></a>
+                            </div>
+                        </div>
+                        <div class="col-md-9 hidden-xs hidden-sm">
+                            <div class="main-menu">
+                                <nav class="nav-menu">
+                                    <ul>
+                                        <li class="active"><a href="#home">Home</a></li>
+                                        <li><a href="#sobre">Sobre</a></li>
+                                        <li><a href="#clientes">Clientes</a></li>
+                                        <li><a href="#contact">Contato</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-xs-12 visible-sm visible-xs">
+                            <div class="mobile_menu"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header area end -->
+    <!-- slider area start -->
+    <section id="home" class="slider-area image-background parallax" data-speed="3" data-bg-image="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/img/bg/slider-bg.jpg">
+        <div class="container">
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="row">
+					<div class="slider-inner text-left">
+						<h2>baixe agora!</h2>
+						<h5><span class="textYellow">Uma amostra de papel de parede</span> em alta resolução pronta para impressão</h5>
+						<h6>Faça seu cadastro e receba os arquivos por E-mail.</h6>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="contact-form">
+					<?php echo do_shortcode('[contact-form-7 id="5" title="Formulário de contato 1"]');?>
+				</div>
+			</div>
+        </div>
+    </section>
+    <!-- slider area end -->
+    <!-- about area start -->
+    <section id="sobre" class="about-area ptb--50">
+        <div class="container">
+           
+            <div class="row d-flex flex-center">
+                <div class="col-md-6 col-sm-6 hidden-xs">
+                    <div class="about-left-img">
+                        <img src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/img/about/abt-left-img.png" alt="image">
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-sm-6 col-xs-12 d-flex flex-left">
+                    <div class="about-content">
+						<div class="section-title">
+							<h2>Arquivo Profissional</h2>
+						</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed  eiuiosmod terttmpor incididunt ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. doliuor in reprehenderit in voluptate velit esse  dolore eu fugiat nulla pariatur. cdatat non proident</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tuiempor incididunt ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about area end -->
+    <!-- testimonial carousel area start -->
+    <section id="clientes" class="testimonial-area ">
+        <div class="container">
+            <div class="section-title">
+                <h2>O que dizem nossos clientes</h2>
+                <p>Nemo enim ipsam voluptatem quia voluptas sit</p>
+            </div>
+            <div class="testimonial-slider owl-carousel">
+                <div class="single-tst">
+                    <img src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/img/author/tst-author1.jpg" alt="author">
+                    <h4>Luana</h4>
+                    <span>ClickArts</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                </div>
+                <div class="single-tst">
+                    <img src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/img/author/tst-author2.jpg" alt="author">
+                    <h4>Cliente 2</h4>
+                    <span>Empresa</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                </div>
+                <div class="single-tst">
+                    <img src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/img/author/tst-author1.jpg" alt="author">
+                    <h4>Cliente 3</h4>
+                    <span>Empresa</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                </div>
+                <div class="single-tst">
+                    <img src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/img/author/tst-author2.jpg" alt="author">
+                    <h4>Cliente 4</h4>
+                    <span>Empresa</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- testimonial carousel area end -->
+    <!-- contact area start -->
+    <section class="contact-area ptb--100" id="contact">
+        <div class="container">
+            <div class="section-title">
+                <h2>Fale com a gente</h2>
+                <!-- <p>Nemo enim ipsam voluptatem quia voluptas sit</p> -->
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="contact-form">
+                        <?php echo do_shortcode('[contact-form-7 id="6" title="Formulario Fim da pagina"]');?>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="contact_info">
+                        <div class="s-info">
+                            <i class="fa fa-map-marker"></i>
+                            <div class="meta-content">
+                                <span>Rua Francisca Nogueira 104</span>
+                            </div>
+                        </div>
+                        <div class="s-info">
+                            <i class="fa fa-mobile"></i>
+                            <div class="meta-content">
+                                <span>14 991886021</span>
+                                <span>+0123 456 789 78</span>
+                            </div>
+                        </div>
+                        <div class="s-info">
+                            <i class="fa fa-paper-plane"></i>
+                            <div class="meta-content">
+                                <span>contato@paulorazz.com.br</span>
+                            </div>
+                        </div>
+                        <div class="c-social">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- contact area end -->
+    <!-- footer area start -->
+    <footer>
+        <div class="footer-area">
+            <div class="container">
+                <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos os direitos reservados | Feito com muito <i class="fa fa-heart-o" aria-hidden="true"></i> por <a href="https://agencianewiscool.com.br" target="_blank">New is Cool</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            </div>
+        </div>
+    </footer>
+    <!-- footer area end -->
+
+    <!-- Scripts -->
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/jquery-3.2.0.min.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/jquery-ui.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/bootstrap.min.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/jquery.slicknav.min.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/owl.carousel.min.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/magnific-popup.min.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/counterup.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/jquery.waypoints.min.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/jquery.mb.YTPlayer.min.js"></script>
+    <script src="http://localhost/paulorazz/wp-content/themes/twentynineteen/assets/js/theme.js"></script>
+</body>
+
+</html>
